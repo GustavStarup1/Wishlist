3b69fb6e4fb756bf8a0a345f6b2ed4f6a90b1cb1
gustavstarup <gust0001@stud.kea.dk>
2024-04-08 11:34:35 +0200

    Index.html and HomeController added

---
+++ src/main/java/com/example/wishlist/controllers/HomeController.java
@@ -0,0 +1,12 @@
+package com.example.wishlist.controllers;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String index(){
+        return "home/index";
+    }
+}
