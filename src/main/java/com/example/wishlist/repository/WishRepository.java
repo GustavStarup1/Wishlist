c4880728cf68f7f5f6a15e4993d521f37ca46b7b
gustavstarup <gust0001@stud.kea.dk>
2024-04-08 12:47:16 +0200

    new build

--- src/main/java/com/example/wishlist/repository/WishRepository.java
+++ src/main/java/com/example/wishlist/repository/WishRepository.java
@@ -1,4 +1,11 @@
 package com.example.wishlist.repository;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+@Repository
 public class WishRepository {
+    @Autowired
+    JdbcTemplate jdbcTemplate;
 }
